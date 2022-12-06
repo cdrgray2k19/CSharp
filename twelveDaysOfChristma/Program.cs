614fc454d3db4635fd2296cf200bfbe6646e7e76
cdrgray2k19 <09cgray@brightoncollege.net>
2022-12-06 13:10:36 +0000

    commit

---
+++ twelveDaysOfChristma/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<double> prices = new List<double>{15.99, 6.59, 4.37, 2.98, 44.99, 8.50, 30, 650, 800, 700, 900, 700};
+double total = 0;
+int presents = 0;
+for (int i = 0; i < prices.Count; i++)
+{
+    int newPresents = (i + 1) * (12 - i);
+    presents += newPresents;
+    total += newPresents * prices[i];
+}
+Console.WriteLine(presents);
+Console.WriteLine(total);
